eaa3df6a57da7e86b3ecc8b0171785c911370a64
Alexandre Miranda <alejunior@msn.com>
2023-06-29 16:16:41 -0300

    ajuste generatePassword

--- generatePassword.js
+++ generatePassword.js
@@ -1,15 +1,22 @@
 function getPassword() {
   const chars =
-    "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJLMNOPQRSTUVWXYZ";
+    "0123456789ABCDEFGHIJKLMnopqrstuvwxyz";
+
   const passwordLength = 6;
+
   let password = "";
 
   for (let i = 0; i < passwordLength; i++) {
     const randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
+    if (i == 0) {
+      password += chars.substring(randomNumber, randomNumber + 1).toUpperCase();
+    } else {
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
   }
 
   console.log("password => ", password);
+
 }
 
 getPassword();
