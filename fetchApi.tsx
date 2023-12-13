9559af67a1a030dd33744b755696fcf4c1a24f37
alejuniormsn <alejunior@msn.com>
2023-12-13 07:49:48 -0300

    commit fetchApi

--- fetchApi.tsx
+++ fetchApi.tsx
@@ -1,22 +1,36 @@
-async function getUser(id: number){
-	const response = await fetch(`http://192.168.1.199:3333${uri}`, {
-  	method: method,
+type Props = {
+  uri: string;
+  method: string;
+  payload?: {};
+  token?: string;
+};
+
+export async function api({
+  uri,
+  method,
+  payload,
+  token,
+}: Props): Promise<any> {
+  const response = await fetch(`http://192.168.1.199:3333${uri}`, {
+    method: method,
     credentials: "same-origin",
     body:
-  	  method === "GET" || method === "DELETE" ? undefined : JSON.stringify(payload),
-  		headers: {
-       "Content-Type": "application/json",
-       Authorization: `Bearer ${token}`,
-  	  },
-  	});
-	
-	let result = await response.json();
+      method === "GET" || method === "DELETE"
+        ? undefined
+        : JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-	if(!response.ok) {
-		throw new Error(response.error);
-	}
-  
-	result = { ...result, cookie: response.headers.get("set-cookie") };
-  
-	return result;
+  let result = await response.json();
+
+  if (!result.ok) {
+    throw new Error(result.error);
+  }
+
+  result = { ...result, cookie: response.headers.get("set-cookie") };
+
+  return result;
 }
