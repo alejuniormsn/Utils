733c38f4f26a6e18d0af3df50e188b67282b2d35
Alexandre Jr Miranda <49948786+alejuniormsn@users.noreply.github.com>
2024-08-08 07:54:31 -0300

    Add files via upload

--- convertValue.js
+++ convertValue.js
@@ -1,9 +1,37 @@
 const convertValue = (value) => {
   const result =
     value.substring(0, value.length - 2) +
-    "," +
+    "." +
     value.substring(value.length - 2, value.length);
-  return result;
+  return parseFloat(result);
 };
 
-module.exports = { convertValue };
+const convertValueRef = (value) => {
+  const result = value.replace(",", ".");
+  return parseFloat(result);
+};
+
+const currency = (value) => {
+  const formatter = new Intl.NumberFormat("pt-BR", {
+    minimumFractionDigits: 2,
+  });
+  return formatter.format(value);
+};
+// 06022024
+const formatDate = (str) => {
+  return (
+    str.substring(0, 2) + "/" + str.substring(2, 4) + "/" + str.substring(4, 8)
+  );
+};
+// 022024
+const formatDateComp = (str) => {
+  return str.substring(0, 2) + "/" + str.substring(2, 6);
+};
+
+module.exports = {
+  convertValue,
+  convertValueRef,
+  currency,
+  formatDate,
+  formatDateComp,
+};
